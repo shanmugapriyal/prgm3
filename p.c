ae2d67f130f938108be285c81b1b66aa3c5f0fbd
shanmugapriyal <priyashre97@gmail.com>
2016-07-21 12:32:26 +0530

    Create p.c

---
+++ p.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c,d,e;
+printf("Enter the calculation:");
+scanf("%d",&a);
+printf("Enter the n value");
+scanf("%d",&b);
+printf("Enter the pair:");
+scanf("%d",&c);
+for(d=0;d<2*c;d++);
+{
+for(e=1;e=2*b-a;e++)
+if(c!=e)
+{
+printf("Enter the pair",c);
+printf("Enter the for day",e++,c);
+}
+}
+printf("Enter the calculation",a);
+return 0;
+}
